958c41c724d8f26104bfa97b7438af7a04b6f706
grimmon@bluemetal.com <grimmon@bluemetal.com>
2016-09-27 09:22:55 -0400

    Created Services

---
+++ ResourcePlanner.Web/Controllers/ResourceDetailController.cs
@@ -0,0 +1,51 @@
+﻿using ResourcePlanner.Services.DataAccess;
+using ResourcePlanner.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ResourcePlanner.Services.Controllers
+{
+    public class ResourceDetailController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(int? ResourceIdParam = null, DateTime? StartDateParam = null, DateTime? EndDateParam = null)
+        {
+            DateTime StartDate;
+            DateTime EndDate;
+            int ResourceId;
+            if (StartDateParam == null || (EndDateParam == null || ResourceIdParam == null))
+            {
+                StartDate = DateTime.Now.AddMonths(-1);
+                EndDate = DateTime.Now.AddMonths(1);
+                ResourceId = 38379;
+            }
+            else
+            {
+                StartDate = StartDateParam.Value;
+                EndDate = EndDateParam.Value;
+                ResourceId = ResourceIdParam.Value;
+            }
+
+            var access = new ResourceDataAccess(ConfigurationManager.ConnectionStrings["RPDBConnectionString"].ConnectionString,
+                                                Int32.Parse(ConfigurationManager.AppSettings["DBTimeout"]));
+            DetailPage detailPage;
+
+            try
+            {
+                detailPage = access.GetResourceDetail(ResourceId, StartDate, EndDate);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return Ok(detailPage);
+        }
+    }
+}
---
+++ ResourcePlanner.Web/DataAccess/ResourceDataAccess.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ResourcePlanner.Services.Models;
+using ResourcePlanner.Services.Utilities;
+using ResourcePlanner.Services.Mapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ResourcePlanner.Services.DataAccess
+{
+    public class ResourceDataAccess
+    {
+        private readonly string _connectionString;
+        private readonly int _timeout;
+
+        public ResourceDataAccess(string connectionString, int timeout)
+        {
+            _connectionString = connectionString;
+            _timeout = timeout;
+        }
+
+
+        public ResourcePage GetResourcePage(DateTime StartDate, DateTime EndDate)
+        {
+
+            var returnValue = AdoUtility.ExecuteQuery(reader => EntityMapper.MapToResourcePage(reader),
+                _connectionString,
+                @"rpdb.ResourcePageSelect",
+                CommandType.StoredProcedure,
+                _timeout,
+                CreateResourcePageParamArray(StartDate, EndDate));
+            return returnValue;
+        }
+
+        public DetailPage GetResourceDetail(int ResourceId, DateTime StartDate, DateTime EndDate)
+        {
+            var returnValue = AdoUtility.ExecuteQuery(reader => EntityMapper.MapToResourceDetail(reader),
+                _connectionString,
+                @"rpdb.ResourceDetailSelect",
+                CommandType.StoredProcedure,
+                _timeout,
+                CreateResourceDetailParamArray(ResourceId, StartDate, EndDate));
+            return returnValue;
+        }
+
+        private SqlParameter[] CreateResourcePageParamArray(DateTime StartDate, DateTime EndDate)
+        {
+            var StartDateParam = AdoUtility.CreateSqlParameter("StartDateParam", SqlDbType.Date, StartDate);
+            var EndDateParam = AdoUtility.CreateSqlParameter("EndDateParam", SqlDbType.Date, EndDate);
+
+            return new SqlParameter[] { StartDateParam, EndDateParam };
+        }
+
+        private SqlParameter[] CreateResourceDetailParamArray(int ResourceId, DateTime StartDate, DateTime EndDate)
+        {
+            var StartDateParam = AdoUtility.CreateSqlParameter("StartDateParam", SqlDbType.Date, StartDate);
+            var EndDateParam = AdoUtility.CreateSqlParameter("EndDateParam", SqlDbType.Date, EndDate);
+            var ResourceIdParam = AdoUtility.CreateSqlParameter("ResourceId", SqlDbType.Int, ResourceId);
+
+            return new SqlParameter[] { StartDateParam, EndDateParam, ResourceIdParam };
+        }
+    }
+}
